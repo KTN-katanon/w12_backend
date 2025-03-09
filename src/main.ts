07d3ac1fac5afe90895e6351b111e676efe4e15a
Katanon Krengsomrong <66160196@go.buu.ac.th>
2025-03-10 00:55:37 +0700

    Add BearerAuth in main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
     .setDescription('The Coffee API description')
     .setVersion('1.0')
     .addTag('coffee')
+    .addBearerAuth()
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
